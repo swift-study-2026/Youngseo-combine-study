219ea44e467b3a29147cdf74296b1a344ebe7eea
Cho Youngseo <jjwm10625@sookmyung.ac.kr>
2026-04-26 15:12:21 +0900

    [setting] 실습 파일 세팅

---
+++ combine_practice/combine_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  combine_practice
+//
+//  Created by youngseo on 4/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
